8b2d30d7d31d4e4527585734739cf13ebb1ac23a
alvin koech <alvinkoech2020@gmail.com>
2025-10-02 13:33:24 +0300

    added js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+// Declare variables in order
+let name = "Susan"; // must be Susan
+let height = 30; // between 0 and 40
 
-module.exports = {name, height, message};
+// Create message AFTER name and height exist
+let message = `${name} is ${height} inches tall`;
+
+// Export so tests can use them
+module.exports = { name, height, message };
